6c104bc6bf579f00c73158456da8c6d6f9094892
STS669-HEMATHKUMAR T <STS669-HEMATHKUMAR T@STS669L-HEMATHK>
2022-10-18 16:18:17 +0530

    Add project files.

---
+++ WebApplication23/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication23.Models;
+
+
+namespace WebApplication23.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        TaskContext _taskContext;
+        public HomeController( TaskContext taskContext)
+        {
+          
+            _taskContext= taskContext;
+        }
+
+       
+        public IActionResult First()
+        {
+            return View();
+        }
+
+        public IActionResult Next(Details details)
+        {
+            Details hk= new Details();
+            hk.Id = details.Id;
+            hk.Name=details.Name;
+            hk.Age=details.Age;
+            hk.Salary=details.Salary;
+            _taskContext.Add(hk);
+            _taskContext.SaveChanges();
+
+            return RedirectToAction("First");
+        }
+
+       
+    }
+}
